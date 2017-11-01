ac8d6c7a341c387223cfdae75132f9c4c241eac5
Conor Parrish <conor.parrish@icloud.com>
2017-11-01 15:30:56 -0600

    Add vent picture and work on fixing email form

--- js/canvas.js
+++ js/canvas.js
@@ -1,8 +1,19 @@
 var c = document.getElementById("demoCanvas");
 var ctx = c.getContext("2d");
 ctx.moveTo(0,0);
-ctx.lineTo(200,0);
-ctx.lineTo(200,200);
-ctx.lineTo(0,200);
+ctx.lineTo(150,0);
+ctx.lineTo(150,150);
+ctx.lineTo(0,150);
 ctx.lineTo(0,0);
+ctx.fillStyle = "white";
+ctx.fillRect(0,0,150,150);
+ctx.stroke();
+
+ctx.moveTo(150,40);
+ctx.lineTo(300,40);
+ctx.lineTo(300,190);
+ctx.lineTo(0,190);
+ctx.lineTo(0,0);
+// ctx.fillStyle = "white";
+// ctx.fillRect();
 ctx.stroke();
